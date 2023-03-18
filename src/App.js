c168a27842856733ac3c182c253f1e42c183e197
smitapatare1 <smitapatare2002@gmail.com>
2023-03-18 13:07:27 +0530

    basic setup completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./app.css"
 
-function App() {
-  return (
-
-    
-
-  );
+function App(){
+  return(
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
